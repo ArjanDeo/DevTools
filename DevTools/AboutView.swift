6d8f3155e7309b87e5a969a86cf0f06cf3a41507
Arjan Deo <arjandeo@bluecomet-it.com>
2026-08-15 20:38:08 +0100

    Improved and made UI consistent across all views.

---
+++ DevTools/AboutView.swift
@@ -0,0 +1,18 @@
+//
+//  AboutView.swift
+//  DevTools
+//
+//  Created by Arjan Deo on 15/08/2026.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AboutView()
+}
