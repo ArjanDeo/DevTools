08e4a96c4e075b1b7188acfa83242ecc89470d48
Arjan Deo <arjandeo@bluecomet-it.com>
2026-08-15 19:20:50 +0100

    Initial Commit

---
+++ DevTools/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DevTools
+//
+//  Created by Arjan Deo on 15/08/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DevTools/DevToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevToolsApp.swift
+//  DevTools
+//
+//  Created by Arjan Deo on 15/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct DevToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
